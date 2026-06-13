59126315683aba41779f922304ed025b4c34e242
Pedro Padilla <peterpark29c1@gmail.com>
2026-06-13 15:17:04 -0600

    infra: introduce terraform remote state and oicd

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "ead-api-tf-state"
+    key            = "ead-api/terraform.tfstate"
+    region         = "us-east-2"
+    use_lockfile   = true
+    dynamodb_table = "ead-api-tf-locks"
+    encrypt = true
+  }
+}
