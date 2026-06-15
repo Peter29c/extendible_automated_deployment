7d0b4813f30f572e61bca56cb0a8a74f249b321e
Pedro Padilla <peterpark29c1@gmail.com>
2026-06-15 11:59:04 -0600

    infra: provision eks cluster pt.1

---
+++ terraform/eks.tf
@@ -0,0 +1,35 @@
+resource "aws_eks_cluster" "main" {
+  name     = "ead-api-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "default"
+
+  node_role_arn = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = data.aws_subnets.default.ids
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_readonly
+  ]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "ecr_repository_url" {
   value = aws_ecr_repository.ead-api.repository_url
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.main.name
+}
